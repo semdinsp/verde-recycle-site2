7d6c3c1e32e1bfa2c9d5184b1ff53db82bbf221a
semdinsp <scott.sproule@ficonab.com>
2020-07-07 21:38:47 +0800

    minor upgardes for first deploy

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -17,28 +17,32 @@ const ContactPage = ({
       <div className="two-grids -contact">
         <div className="post-thumbnail" style={{backgroundImage: `url('/assets/alexander-andrews-HgUDpaGPTEA-unsplash.jpg')`, marginBottom: 0}}>
           <h1 className="post-title">Get in Touch</h1>
-          <p>Let me help you kick start your next project &rarr;</p>
+          <p>Contact us to work with us further in our various projects especially recyclinng and marketing services.  We are always looking to explore partnerships. &rarr;</p>
         </div>
         <div>
-          <form className="form-container" action="https://sendmail.w3layouts.com/SubmitContactForm" method="post">
-            <div>
-              <label htmlFor="w3lName">Name</label>
-              <input type="text" name="w3lName" id="w3lName"/>
+          <form className="form-container" name="contact-recycle" data-netlify="true"  data-netlify-honeypot="bot-field" action="/success"  method="POST">
+            <div>  
+              <input type="hidden" name="bot-field" />
+              <input type="hidden" name="form-name" value="contact-recycle" />
             </div>
             <div>
-              <label htmlFor="w3lSender">Email</label>
-              <input type="email" name="w3lSender" id="w3lSender"/>
+              <label htmlFor="name">Name
+              <input type="text" name="name" id="name" /></label>
             </div>
             <div>
-              <label htmlFor="w3lSubject">Subject</label>
-              <input type="text" name="w3lSubject" id="w3lSubject"/>
+              <label htmlFor="email">Email
+              <input type="email" name="email" id="sender" /></label>
             </div>
             <div>
-              <label htmlFor="w3lMessage">Message</label>
-              <textarea name="w3lMessage" id="w3lMessage"></textarea>
+              <label htmlFor="subject">Subject
+              <input type="text" name="subject" id="subject" /></label>
+            </div>
+            <div>
+              <label htmlFor="message">Message
+              <textarea name="message" id="message"> </textarea></label>
             </div>
             <div style={{display: "flex", justifyContent: "flex-end"}}>
-              <input type="submit" className="button -primary" style={{marginRight: 0}} />
+              <button type="submit" className="button -primary" style={{marginRight: 0}}>Send</button>
             </div>
           </form>
         </div>
