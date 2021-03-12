91941514f6c497b14dba96da640cd03d194d1c9e
semdinsp <scott.sproule@ficonab.com>
2021-03-12 21:16:14 +0800

    removed consolelog statement

--- src/components/logo.js
+++ src/components/logo.js
@@ -1,6 +1,5 @@
 import React from "react"
 import logo from "../images/verdelogo.png" // Tell webpack this JS file uses this image
-console.log(logo) // /logo.84287d09.png
 function HeaderLogo() {
   // Import result is the URL of your image
   return <img src={logo} alt="Al Eka Fishworld Logo" placeholder="blurred" width={40} height={40}  />
